e9c66e6afc2944d941578f2adef48346815c9fb7
Abhisek753 <properachievement@gmail.com>
2025-12-15 20:08:48 +0530

    First class commit

---
+++ BACKEND/DAYONE/index.js
@@ -0,0 +1,7 @@
+// console.log("Hi Nodejs")
+console.log(global);
+
+console.log(__dirname);
+console.log(__filename);
+console.log(process.pid);
+console.log(process.version);
---
+++ test/index.js
@@ -0,0 +1,50 @@
+// // console.log("data call function")
+// // let data=fetch("https://jsonplaceholder.typicode.com/posts/1").then(res=>res.json()).then(res=>console.log(res,"-----call"));
+// // console.log(data,"data")
+
+
+// let p = new Promise((resolve, reject) => {
+
+//   setTimeout(() => {
+//     resolve("Done!");
+//     // or reject("Error!")
+//   }, 2000);
+// });
+// console.log(p,"second")
+// p.then(res => {
+//   console.log("Promise fulfilled:", res);
+// }).catch(err => {
+//   console.log("Promise rejected:", err);
+// });
+
+
+const path = require("path");
+
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.dirname(__filename));
+// const os = require("os");
+
+// console.log(os.platform());
+// console.log(os.cpus().length);
+// console.log(os.totalmem());
+
+// console.log(global);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.pid);
+// console.log(process.version);
+
+// let val=path.join(__dirname, "logs", "server", "log.txt");
+// console.log(val)
+// const os=require("os")
+// console.log(os.platform());
+// console.log(os.cpus().length);
+// console.log(os.freemem());
+
+
+// const fs=require("fs")
+
+// const content = fs.readFileSync("a.txt", "utf8");
+// console.log(content);
+// fs.readFileSync("a.txt","utf8");
